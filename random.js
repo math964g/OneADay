3cb3fa57f877c8fa255a45ea0f9f9305aa5f2da5
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-09-30 01:04:39 +0200

    Started working on the random challenge return

---
+++ random.js
@@ -0,0 +1,26 @@
+console.log("random.js loaded");
+
+function getRandomChallenge()
+{
+    var challenges = [
+        "Do a silly face to a random stranger",
+        "Stop procastinating",
+        "Do it, TODAY",
+        "Don't drink the water in the wishing well"
+    ];
+
+    // return challenges[Math.floor(Math.random() * names.length)];
+    var loadedchallenge = challenges[Math.floor(Math.random() * names.length)];
+
+    var output = document.getElementById('challengeoutput');
+    output.innerHTML = loadedchallenge;
+}
+
+getRandomChallenge();
+// document.write( getRandomName() );
+//
+//
+// function clickchange() {
+//         var ptag = document.getElementById('par1');
+//         ptag.innerHTML = 'Observe the Change';
+//     }
