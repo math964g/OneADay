f1bb2ad876ca3f0e5bbbcabda0fe5d878c9ab938
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-10-02 17:39:07 +0200

    Added challenges

--- random.js
+++ random.js
@@ -1,25 +1,55 @@
 function getRandomChallenge()
 {
     var challenges = [
-        "Do a silly face to a random stranger",
-        "Stop procastinating",
-        "Do it, TODAY",
-        "Don't drink the water in the wishing well"
+        "&#127881; Go clubbing alone or with strangers &#127881;",
+        "Stop procastinating &#9203;",
+        "Do it, TODAY &#128581;",
+        "Compliment a person today",
+        "Be at work/study without posing",
+        "Jump into a huge body of water (BONUS at night)",
+        "Meetup with a group of strangers",
+        "Confess true feelings to someone, good or bad",
+        "Paint an abstract colorful piece",
+        "Plan your next tattoo and show it by midnight",
+        "Write a song about feelings",
+        "Go sing karaoke",
+        "Compliment a STRANGER today",
+        "Plan where you are in life in 2 years, and share it",
+        "Buy that one thing you don't need but really desire",
+        "Express gratitude of 3 things to someone",
+        "Go hiking",
+        "Nudity. Cause skinny dipping is fun",
+        "Clubbing without alchohol",
+        "Buying something ridiculously stupid",
+        "Sing loudly and proudly",
+        "Build something",
+        "Write something",
+        "Paint something",
+        "Wear the yes hat for a day",
+        "Color part of your hair in a different color",
+        "Shout at the top of your lungs",
+        "Overcome a fear",
+        "Find an idea",
+        "Wear a silly outfit",
+        "Wear an out of character outfit",
+        "Don't drink the water in the wishing well &#128689;",
+        "Do a 20 minute workout &#127947;"
     ];
 
-    // return challenges[Math.floor(Math.random() * names.length)];
+    // Picks a random challenge
     var loadedchallenge = challenges[Math.floor(Math.random() * challenges.length)];
 
+
     var output = document.getElementById("boxtxt").innerHTML;
+
+    var prev = loadedchallenge;
+
     var res = output.replace("Contact maintenance", loadedchallenge);
     document.getElementById("boxtxt").innerHTML = res;
+
+    // TODO: Need to implement a function that switches the challenge every 24-hour instead of on-load
+    // TODO: Needs to make sure a challenge isn't repeated until 3 days or so after it's first apperance
+    // IDEA: Add a timeframe of when the challenge has to be completed - Not just 24 hours, but some of them maybe 3 days - Allows you to plan the challenge and execute it if it's more complicated
 }
 
 getRandomChallenge();
-// document.write( getRandomName() );
-//
-//
-// function clickchange() {
-//         var ptag = document.getElementById('par1');
-//         ptag.innerHTML = 'Observe the Change';
-//     }
