1bb84bd21734f79bd16d5ba999abdbdb4523a73e
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-10-04 09:18:49 +0200

    changed variable names

--- random.js
+++ random.js
@@ -88,7 +88,7 @@ var x = setInterval(countingClock, 1000);
 
 
 function countingClock() {
-  
+
   // Get today's date and time
   var now = new Date().getTime();
 
@@ -102,12 +102,12 @@ function countingClock() {
   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
 
   // Output the result in an element with id="demo"
-  document.getElementById("demo").innerHTML = days + "d " + hours + "h "
+  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
   + minutes + "m " + seconds + "s ";
 
   // If the count down is over, write some text
   if (distance < 0) {
     clearInterval(x);
-    document.getElementById("demo").innerHTML = "EXPIRED";
+    document.getElementById("clock").innerHTML = "EXPIRED";
   }
 }
