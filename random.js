aec5ce4cc3d23d3ffec15079cf59e6a9804e1827
MathiasGN <43538343+math964g@users.noreply.github.com>
2019-10-06 17:50:32 +0200

    dunno

--- random.js
+++ random.js
@@ -83,7 +83,7 @@ audio.onended = function() {
 // The 24-hour countdown clock
 
 // Set the date we're counting down to
-var countDownDate = new Date("Jan 5, 2021 15:37:25").getTime();
+var countDownDate = new Date("2020 15:37:25").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(countingClock, 1000);
