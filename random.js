8d13f83ef4bfa03caac8c6694d6464204f441b03
MathiasGN <math964g@edu.ucl.dk>
2019-10-15 12:51:12 +0200

    clered old cock

--- random.js
+++ random.js
@@ -67,6 +67,7 @@ function getRandomChallenge() {
 
 getRandomChallenge();
 
+// Audio player
 var audio = new Audio("audio/vibes.mp3");
 
 audio.oncanplaythrough = function() {
@@ -85,36 +86,36 @@ audio.onended = function() {
 // Set the date we're counting down to
 // var countDownDate = new Date("2020 15:37:25").getTime();
 
-var today = new Date();
-// var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-var countDownDate = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
-// Update the count down every 1 second
-var x = setInterval(countingClock, 1000);
-
-
-function countingClock() {
-
-  // Get today's date and time
-  var now = new Date().getTime();
-
-  // Find the distance between now and the count down date
-  var distance = countDownDate - now;
-
-  // Time calculations for days, hours, minutes and seconds
-  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-  // Output the result in an element with id="demo"
-  document.getElementById("clock").innerHTML = days + "d " + hours + "h "
-  + minutes + "m " + seconds + "s ";
-
-  // If the count down is over, write some text
-  if (distance < 0) {
-    clearInterval(x);
-    document.getElementById("clock").innerHTML = "EXPIRED";
-    getRandomChallenge();
-  }
-}
+// var today = new Date();
+// // var time = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+// var countDownDate = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+//
+// // Update the count down every 1 second
+// var x = setInterval(countingClock, 1000);
+//
+//
+// function countingClock() {
+//
+//   // Get today's date and time
+//   var now = new Date().getTime();
+//
+//   // Find the distance between now and the count down date
+//   var distance = countDownDate - now;
+//
+//   // Time calculations for days, hours, minutes and seconds
+//   var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+//   var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+//   var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+//   var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+//
+//   // Output the result in an element with id="demo"
+//   document.getElementById("clock").innerHTML = days + "d " + hours + "h "
+//   + minutes + "m " + seconds + "s ";
+//
+//   // If the count down is over, write some text
+//   if (distance < 0) {
+//     clearInterval(x);
+//     document.getElementById("clock").innerHTML = "EXPIRED";
+//     getRandomChallenge();
+//   }
+// }
